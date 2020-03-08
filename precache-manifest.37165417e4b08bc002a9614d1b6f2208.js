5d4fa0030409ca1111b43c421dbb41526e89b7c4
alexandre-mace <contact.alexandremace@gmail.com>
2020-03-08 13:05:29 +0100

    Updates

---
+++ precache-manifest.37165417e4b08bc002a9614d1b6f2208.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9065a3898a21e4141d5be7b7733cf492",
+    "url": "/together/index.html"
+  },
+  {
+    "revision": "4aba651866c1134f96d3",
+    "url": "/together/static/css/2.62f44f39.chunk.css"
+  },
+  {
+    "revision": "77384239ddbfaac41598",
+    "url": "/together/static/css/main.512f3479.chunk.css"
+  },
+  {
+    "revision": "4aba651866c1134f96d3",
+    "url": "/together/static/js/2.d7fc5735.chunk.js"
+  },
+  {
+    "revision": "6a625bc547143ae276effb8c97600963",
+    "url": "/together/static/js/2.d7fc5735.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77384239ddbfaac41598",
+    "url": "/together/static/js/main.5bc337ff.chunk.js"
+  },
+  {
+    "revision": "6d4b233525bda344f165",
+    "url": "/together/static/js/runtime-main.05a2256f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/together/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/together/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/together/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/together/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/together/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
