5d7e3a7bcae63f71075724a7f9998a39df5a0eb4
alexandre-mace <contact.alexandremace@gmail.com>
2020-03-27 15:34:53 +0100

    Add show password feature on registering

--- client/src/components/auth/molecules/WizardUserFormThirdPage.js
+++ client/src/components/auth/molecules/WizardUserFormThirdPage.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import {ErrorMessage, Field} from "formik";
 import {TextField} from "formik-material-ui";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 
-export default (props) => (
-  <div>
-    <div className="form-group">
-      <Field component={TextField} variant={"filled"} label="Mot de passe" margin="normal" fullWidth name="password" type="password" />
-      <ErrorMessage name="password" component="div" className="invalid-feedback" />
+export default (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  return (
+    <div>
+      <div className="form-group">
+        <div className="d-flex position-relative">
+          <Field component={TextField} variant={"filled"} label="Mot de passe" margin="normal" fullWidth name="password"
+                 type={showPassword ? 'text' : 'password'}
+          />
+          <VisibilityIcon className={"show-password"} onClick={() => setShowPassword(!showPassword)} color={showPassword ? "primary" : "disabled"}/>
+          <ErrorMessage name="password" component="div" className="invalid-feedback"/>
+        </div>
+      </div>
+      <div className="form-group">
+        <div className="d-flex position-relative">
+          <Field component={TextField} variant={"filled"} label="Confirmez le mot de passe" margin="normal" fullWidth
+                 name="confirmPassword" type={showConfirmPassword ? 'text' : 'password'}
+          />
+          <VisibilityIcon className={"show-password"} onClick={() => setShowConfirmPassword(!showConfirmPassword)} color={showConfirmPassword ? "primary" : "disabled"}/>
+        </div>
+        <ErrorMessage name="confirmPassword" component="div" className="invalid-feedback"/>
+      </div>
     </div>
-    <div className="form-group">
-      <Field component={TextField} variant={"filled"} label="Confirmez le mot de passe" margin="normal" fullWidth name="confirmPassword" type="password" />
-      <ErrorMessage name="confirmPassword" component="div" className="invalid-feedback" />
-    </div>
-  </div>
-);
+  )
+};
